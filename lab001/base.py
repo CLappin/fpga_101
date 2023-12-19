b1151ac2c0e17ce151fe0ee3e1cf1feb844de9f9
Ciaran Lappin <ciaran.lappin@microchip.com>
2023-12-19 12:04:38 +0000

    Retargetting FPGA device to Future Electronics Avalanche board, containing the Microchip PolarFire FPGA.

--- lab001/base.py
+++ lab001/base.py
@@ -3,30 +3,30 @@
 from migen import *
 
 from litex.build.generic_platform import *
-from litex.build.xilinx import XilinxPlatform
+from litex.build.microsemi import MicrosemiPlatform
 
 # IOs ----------------------------------------------------------------------------------------------
 
 _io = [
-    ("user_led",  0, Pins("H17"), IOStandard("LVCMOS33")),
+    ("user_led",  0, Pins("D6"), IOStandard("LVCMOS33")),
 
-    ("user_sw",  0, Pins("J15"), IOStandard("LVCMOS33")),
+    ("user_sw",  0, Pins("E14"), IOStandard("LVCMOS33")),
 
-    ("user_btn", 0, Pins("N17"), IOStandard("LVCMOS33")),
+    ("user_btn", 0, Pins("E13"), IOStandard("LVCMOS33")),
 
-    ("clk100", 0, Pins("E3"), IOStandard("LVCMOS33")),
+    ("clk100", 0, Pins("R1"), IOStandard("LVCMOS33")),
 
-    ("cpu_reset", 0, Pins("C12"), IOStandard("LVCMOS33")),
+    ("cpu_reset", 0, Pins("F5"), IOStandard("LVCMOS33")),
 ]
 
 # Platform -----------------------------------------------------------------------------------------
 
-class Platform(XilinxPlatform):
+class Platform(MicrosemiPlatform):
     default_clk_name   = "clk100"
     default_clk_period = 1e9/100e6
 
     def __init__(self):
-        XilinxPlatform.__init__(self, "xc7a100t-csg324-1", _io, toolchain="vivado")
+        MicrosemiPlatform.__init__(self, "MPF300TS-FCG484-1", _io, toolchain="libero_soc_polarfire")
 
 # Design -------------------------------------------------------------------------------------------
 
